52499f77c8e8bfcc2eb781c42ac7e039f0156586
Henry Senior <hps1945@live.co.uk>
2017-02-02 16:23:15 +0000

    initial repo set up

---
+++ TouchTable/Touch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchTable
+{
+    /// <summary>
+    /// A class representing the touch input and output from the Touch Table
+    /// </summary>
+    public class Touch
+    {
+        /*Method list
+         *  -Get X, Y of touch input
+         *  -Get double tap (used for click)
+         *  -Get hold down (right click?)
+         */
+    }
+}
---
+++ TouchTable/gesture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchTable
+{
+    /// <summary>
+    /// A class representing all the gestures that are possible using the Touch Table
+    /// </summary>
+    class gesture
+    {
+        /*
+         * Method List
+         *  -Pinch zoom in
+         *  -Pinch zoom out
+         *  4 finger swipe down
+         */
+    }
+}
